42889d78b36dd03d2b9c9c7fde4b7b9371de1a04
David Bassey <davidosky1@gmail.com>
2020-11-02 20:45:31 +0100

    adds readme file and linters file

--- main.rb
+++ main.rb
@@ -25,3 +25,21 @@ end
 arrays = [8, 3, 10, 1, 4, 6, ]
 
 p bubble_sort(arrays)
+
+    def bubble_sort_by(str_arry)
+        str_arry.length.times do
+        str_arry.each_with_index do |value, index|
+            next unless index < str_arry.length - 1
+            sort_value = yield(value, str_arry[index + 1])
+            if sort_value.positive?
+            str_arry[index] = str_arry[index + 1]
+            str_arry[index + 1] = value
+            end
+        end
+        end
+        str_arry
+    end
+    display = bubble_sort_by(%w[hi hello hey]) do
+    |left, right| left.length - right.length 
+    end
+    p display
