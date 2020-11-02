8b04843a5e8c192cf90d7fa8da951747a3047759
uchennaanya <uchennaongit@gmail.com>
2020-11-02 21:21:34 +0100

    the second code

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ def bubble_sort(array_list)
     array = array_list.size
 
     if array <= 1
-        return array_list
+        return "Empty list"
     end
 
     loop do 
@@ -22,6 +22,26 @@ end
 array_list
 end
 
-arrays = [8, 3, 10, 1, 4, 6, ]
+arrays = [9, 8, 3, 0, 1]
 
 p bubble_sort(arrays)
+
+def bubble_sort_by(str_array)
+        str_array.each_with_index do |value, index|
+            
+        next unless index < str_array.length - 1
+
+        sort_value = yield(value, str_array[index + 1])
+  
+        if sort_value.positive?
+          str_array[index] = str_array[index + 1]
+          str_array[index + 1] = value
+        end
+    end
+end
+
+  display = bubble_sort_by(%w[hi hello hey]) do
+   |left, right| left.length - right.length 
+end
+
+  p display
