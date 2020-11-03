91c401fc05dbbd02dbd4211ea402dfcb4fe9b4ac
uchennaanya <uchennaongit@gmail.com>
2020-11-03 08:50:19 +0100

    change variable names

--- main.rb
+++ main.rb
@@ -1,32 +1,33 @@
 def bubble_sort(array_list)
-    array = array_list.size
+    array_length = array_list.size
 
-    if array <= 1
+    if array_length <= 1
         return "Empty list"
     end
 
     loop do 
-        _sort = false
+        sort = false
 
-        (array-1).times do |index|
+        (array_length-1).times do |index|
 
     if array_list[index] > array_list[index + 1]
 
         array_list[index], array_list[index + 1] = array_list[index + 1], array_list[index]
 
-        _sort = true
+        sort = true
     end
 end
-break if not _sort
+break if not sort
 end
 array_list
 end
 
-arrays = [9, 8, 3, 0, 1]
+arrays = [4, 6, 10, 1]
 
 p bubble_sort(arrays)
 
     def bubble_sort_by(str_arry)
+        
         str_arry.length.times do
         str_arry.each_with_index do |value, index|
             next unless index < str_arry.length - 1
@@ -39,7 +40,9 @@ p bubble_sort(arrays)
         end
         str_arry
     end
-    display = bubble_sort_by(%w[hi hello hey]) do
-    |left, right| left.length - right.length 
+
+    display = bubble_sort_by(%w[hi hello hey]) do 
+        |left, right| left.length - right.length
     end
+
     p display
