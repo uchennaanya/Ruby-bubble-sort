242e8f2c86ce65547b743628249064aeaff51da1
David Bassey <davidosky1@gmail.com>
2020-11-03 08:45:25 +0100

    fixes linter errors

--- main.rb
+++ main.rb
@@ -1,45 +1,42 @@
 def bubble_sort(array_list)
-    array = array_list.size
+  array = array_list.size
 
-    if array <= 1
-        return "Empty list"
+  return 'Empty list' if array <= 1
+
+  loop do
+    sort = false
+
+    (array - 1).times do |index|
+      next unless array_list[index] > array_list[index + 1]
+
+      array_list[index], array_list[index + 1] = array_list[index + 1], array_list[index]
+
+      sort = true
     end
-
-    loop do 
-        _sort = false
-
-        (array-1).times do |index|
-
-    if array_list[index] > array_list[index + 1]
-
-        array_list[index], array_list[index + 1] = array_list[index + 1], array_list[index]
-
-        _sort = true
-    end
-end
-break if not _sort
-end
-array_list
+    break unless sort
+  end
+  array_list
 end
 
 arrays = [9, 8, 3, 0, 1]
 
 p bubble_sort(arrays)
 
-    def bubble_sort_by(str_arry)
-        str_arry.length.times do
-        str_arry.each_with_index do |value, index|
-            next unless index < str_arry.length - 1
-            sort_value = yield(value, str_arry[index + 1])
-            if sort_value.positive?
-            str_arry[index] = str_arry[index + 1]
-            str_arry[index + 1] = value
-            end
-        end
-        end
-        str_arry
+def bubble_sort_by(str_arry)
+  str_arry.length.times do
+    str_arry.each_with_index do |value, index|
+      next unless index < str_arry.length - 1
+
+      sort_value = yield(value, str_arry[index + 1])
+      if sort_value.positive?
+        str_arry[index] = str_arry[index + 1]
+        str_arry[index + 1] = value
+      end
     end
-    display = bubble_sort_by(%w[hi hello hey]) do
-    |left, right| left.length - right.length 
-    end
-    p display
+  end
+  str_arry
+end
+display = bubble_sort_by(%w[hi hello hey]) do |left, right|
+  left.length - right.length
+end
+p display
